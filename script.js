ab20f70330e6404d8c6bff182b4bccafcef0f6ed
Aishwarya <142329750+A-9514@users.noreply.github.com>
2023-12-17 19:18:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i = shortHex.length-3;
+    let res = "#"
+    let isupper = false
+    while(i<shortHex.length){
+        let aciicode = shortHex[i].charCodeAt(0)
+        if(aciicode>=65 && aciicode<=90){
+            isupper = true;
+        }
+        res = res+shortHex[i]+shortHex[i]
+        i++;
+    }
+    return isupper ? res.toUpperCase() : res;
+};
 };
 
 // Do not change the code below.
